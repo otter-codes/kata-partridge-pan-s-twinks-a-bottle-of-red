076ee931fba652ba4a6938786cb20efb3b5e5fde
Ken Rayner <kenneth.rayner@digital.hmrc.gov.uk>
2018-02-08 12:21:05 +0000

    add part

--- src/main/scala/Partridge.scala
+++ src/main/scala/Partridge.scala
@@ -1,12 +1,19 @@
-object Partridge {
+object Partridge extends App {
 
   // IMPORTANT: Do not change the signatures of the functions declared below.
   // See the README file for explanations of what each function should do.
 
   def part(args: Array[String]): String = {
-    ???
-  }
 
+    val quoteSet = Set("Partridge", "PearTree", "Chat", "Dan", "Toblerone", "Lynn", "AlphaPapa", "Nomad")
+    val q = args.filter((i: String) => quoteSet contains i).toList.length
+    //(s"${q}")
+    if(q== 0){
+      "Lynn, I've pierced my foot on a spike!!"
+    } else {
+      s"Mine's a pint${"!" * q}"
+    }
+  }
   def apple(x: Any): String = {
     ???
   }
@@ -14,5 +21,5 @@ object Partridge {
   def london(args: Array[String]): String = {
     ???
   }
-
+print(part(Array("word")))
 }
