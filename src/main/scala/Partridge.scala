2ed6cf8dab87da831ab3cf6ee7fd3579209f3c6a
Kevin Coatsworth <kevin.coatsworth@digital.hmrc.gov.uk>
2018-02-08 12:41:46 +0000

    London function added for Partidge kata.

--- src/main/scala/Partridge.scala
+++ src/main/scala/Partridge.scala
@@ -1,4 +1,4 @@
-object Partridge {
+object Partridge extends App {
 
   // IMPORTANT: Do not change the signatures of the functions declared below.
   // See the README file for explanations of what each function should do.
@@ -12,7 +12,26 @@ object Partridge {
   }
 
   def london(args: Array[String]): String = {
-    ???
+
+    val londonSet = Set("Rejection", "Disappointment", "Backstabbing Central", "Shattered Dreams Parkway")
+
+    if (londonSet.subsetOf(args.toSet)){
+
+      "Smell my cheese you mother!"
+
+    }
+
+    else {
+
+      "No, seriously, run. You will miss it."
+
+    }
+
   }
+
+  val userArray = Array("Norwich", "Tooting", "Bank",
+    "Rejection", "Disappointment", "Backstabbing Central",
+    "Exeter", "Shattered Dreams Parkway", "Belgium","London")
+  println(london(userArray))
 
 }
