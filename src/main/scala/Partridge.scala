9a2d2480df457ad372a22b11146e8f9436a80344
Liam Henderson <liam.henderson@digital.hmrc.gov.uk>
2018-02-08 13:16:17 +0000

    add apple function

--- src/main/scala/Partridge.scala
+++ src/main/scala/Partridge.scala
@@ -1,4 +1,6 @@
-object Partridge {
+import scala.io.StdIn
+
+object Partridge extends App {
 
   // IMPORTANT: Do not change the signatures of the functions declared below.
   // See the README file for explanations of what each function should do.
@@ -8,11 +10,15 @@ object Partridge {
   }
 
   def apple(x: Any): String = {
-    ???
+    val b = x.toString.toInt
+    if (b * b <= 1000) {
+      "Help yourself to a honeycomb Yorkie for the glovebox."
+    }
+    else "It's hotter than the sun!!"
   }
 
   def london(args: Array[String]): String = {
     ???
   }
-
+  
 }
