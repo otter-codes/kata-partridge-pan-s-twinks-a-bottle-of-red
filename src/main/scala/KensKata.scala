1b74832c68ecbb1d2f3ea808f40a39f4d69a54ad
Ken Rayner <kenneth.rayner@digital.hmrc.gov.uk>
2018-02-26 16:41:24 +0000

    refactor code

--- src/main/scala/KensKata.scala
+++ src/main/scala/KensKata.scala
@@ -1,52 +1,21 @@
-import java.lang
-import java.io.IOException
-import java.lang.NumberFormatException
-
-
 import scala.io.StdIn
-
-import scala.util.{Failure, Success, Try}
-
-
 object KensKata extends  App {
-
   val firstNumber = StdIn.readLine("Please enter first number")
   val secondNumber = StdIn.readLine("Please enter second number")
-
- /*def numbersToInt ( nums:String):Any= {
-   try {
-     nums.toInt
-   } catch {
-     case e: NumberFormatException => "Enter another number"
-   }
- }
- */
-  /*
- def toInt(s: String):Option[Int] = {
-   try {
-     Some(s.toInt)
-   } catch {
-     case e: NumberFormatException => None
-   }
- }
-      case Failure (NumberFormatException) =>
-        println("Enter another number")
-      }
-*/
-
-  def numbers1( nums3: String ): Int = {
-    nums3 match {
+  def num1( nums: String ): Int = {
+    nums match {
       case "" => -1
-      case _ => nums3.toInt
+      case _ => nums.toInt
     }
   }
-  def division( number1: Int, number2: Int ): String = {
-    val result = numbers1(firstNumber) / numbers1(secondNumber)
-    val remainder = numbers1(firstNumber) % numbers1(secondNumber)
-
-    s"Number $firstNumber divided by $secondNumber is $result and has remainder $remainder"
-
-  }
-  println(division(numbers1(firstNumber),numbers1( secondNumber)))
-
+    def division( no1: Int, no2: Int ): String = {
+      if ((num1(firstNumber) == -1) || (num1(secondNumber) == -1)) {
+        "You did not enter a valid number!!!"
+      } else {
+        val result = num1(firstNumber) / num1(secondNumber)
+        val remainder = num1(firstNumber) % num1(secondNumber)
+        s"Number $firstNumber divided by $secondNumber is $result and has remainder $remainder"
+      }
+    }
+      println(division(num1(firstNumber), num1(secondNumber)))
 }
--- src/test/scala/KensKataSpec.scala
+++ src/test/scala/KensKataSpec.scala
@@ -6,15 +6,16 @@ class KensKataSpec extends WordSpec with MustMatchers {
   "numbers1" must {
 
     "return '-1' when  empty string is input" in {
-      KensKata.numbers1("") mustEqual -1
+      KensKata.num1("") mustEqual -1
     }
     "return '1' when  1 is input" in {
-      KensKata.numbers1("1") mustEqual 1
+      KensKata.num1("1") mustEqual 1
     }
     "return '5' when  5 is input" in {
-      KensKata.numbers1("6") mustEqual 6
+      KensKata.num1("6") mustEqual 6
     }
+    // "return 'NumberFormatException1 when anything except a number is entered" in{
 
-    }
-
+    //}
+  }
 }
